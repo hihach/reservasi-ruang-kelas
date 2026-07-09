fb1cfd01764e57b435cd1f4af6ac6bc468648255
VIRTHS12 <187938754+VIRTHS12@users.noreply.github.com.>
2026-07-09 21:24:38 +0700

    FIX BUG SCRIPT5

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Lantai;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
